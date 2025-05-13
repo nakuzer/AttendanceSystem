58828a3007724fdee1066bb5e5dc5f45646d9676
nakuzer <marianabunda3@gmail.com>
2025-05-13 13:40:29 +0800

    Refactored and Added Database
    Real Shit

--- Project_AS/Pages/signUp.php
+++ Project_AS/Pages/signUp.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>CheckLY - Create Account</title>
-  <link rel="stylesheet" href="signup.css">
+  <link rel="stylesheet" href="../style/signup.css">
 </head>
 
 <body>
@@ -16,25 +16,33 @@
     <h2>Choose Your Role</h2>
     <div class="role-select">
       <div class="role-option student" data-role="student">
-        <img src="images/student1.png" alt="">
-          
+        <img src="../images/student1.png" alt="">
         <span>Student</span>
       </div>
       <div class="role-option teacher" data-role="teacher">
-        <img src="images/teacher1.png" alt="">
+        <img src="../images/teacher1.png" alt="">
         <span>Teacher</span>
       </div>
     </div>
 
     <form class="signup-form" id="signupForm">
 
-      <input type="hidden" id="selectedRole" name="role" required>
+      <input type="hidden" id="selectedRole" name="role" value="" required>
 
       <input type="email" id="email" name="email" placeholder="Email" required>
+      <div id="email-error" style="color: red;"></div>
 
-      <input type="text" id="idInput" name="schoolId" placeholder="School ID" required>
+      <input type="text" id="idInput" name="schoolID" placeholder="School ID" required>
 
-      <input type="text" id="courseInput" name="Course" placeholder="Course" required>
+      <input list="courseList" id="courseInput" name="course" placeholder="Course">
+      <datalist id="courseList">
+        <option value="BSIT"></option>
+        <option value="BSCS"></option>
+        <option value="BACT"></option>
+        <option value="BSIS"></option>
+      </datalist>
+      <input type="text" id="fieldInput" name="field" placeholder="Field of Expertise" style="display: none;">
+
 
       <div class="name-inputs">
         <input type="text" id="firstName" name="firstName" placeholder="First Name" required>
@@ -45,11 +53,11 @@
 
       <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" required>
 
-      <button onclick="window.location.href='index.html'" id="submit-btn" type="submit">Create my new account</button>
+      <button id="submit-btn" type="submit">Create my new account</button>
     </form>
   </div>
 
-  <script src="signup.js"></script>
+  <script src="../JavaScript/signup.js"></script>
 </body>
 
 </html>
