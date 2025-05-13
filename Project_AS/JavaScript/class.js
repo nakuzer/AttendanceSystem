4acfd6f67bd78d2ab227956327eb2d57c16fc5a3
Danyxtu <Danyxtu@users.noreply.github.com>
2025-05-13 14:42:08 +0800

    last update(error)

--- Project_AS/JavaScript/class.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('saveBtn').addEventListener('click', function () {
-    const confirmSave = confirm('Are you sure you want to save?');
-    if (confirmSave) {
-        this.textContent = 'Saved';
-        alert('Changes saved successfully!');
-    }
-});
